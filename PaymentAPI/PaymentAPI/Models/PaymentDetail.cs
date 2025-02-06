64e1130f77794675ce8fc4badd861df369d75769
panchanan-turuk <pturuk123@gmail.com>
2025-02-06 12:45:56 +0530

    PaymentsAPI

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwonerName { get; set; } = "";
+        [Column(TypeName = "nvarchar(20)")]
+        public string CarNumber { get; set; } = "";
+        //mm/yy
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpiratoinDate { get; set; } = "";
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+
+
+    }
+}
