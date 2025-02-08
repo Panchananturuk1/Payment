0392406c866c58318812bc7764d74c5a01f1d1a0
panchanan-turuk <pturuk123@gmail.com>
2025-02-08 14:20:51 +0530

    data-fetch

---
+++ PaymentUI/src/app/shared/payment-detail.model.ts
@@ -0,0 +1,8 @@
+export class PaymentDetail {
+
+    paymentDetailId:number = 0
+    cardOwonerName: string = ""
+    carNumber: string = ""
+    expiratoinDate: string = ""
+    securityCode: string = ""
+}
